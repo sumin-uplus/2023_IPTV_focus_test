3da708d64270f9bb4062d220c70a585165ca2d85
sumin <sumin.uplus@gmail.com>
2023-06-14 17:03:39 +0900

    LNB 제작중

---
+++ src/components/test2/LeftNavItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="nav_item_container">
+        <div class="indicator"></div>
+        <img class="icon" :src="{ icon }">
+        <div class="text">{{ text }}</div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "LeftNavItem",
+        props: {
+            icon: { type: String },
+            text: { type: Text }
+        }
+    }
+</script>
