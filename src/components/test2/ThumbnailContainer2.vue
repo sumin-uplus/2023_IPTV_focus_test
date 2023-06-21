025f7f1c0e1e59aa29ea9a8877868d6b8cc73e59
sumin <sumin.uplus@gmail.com>
2023-06-21 10:27:01 +0900

    불필요 코드 삭제

--- src/components/test2/ThumbnailContainer2.vue
+++ src/components/test2/ThumbnailContainer2.vue
@@ -80,86 +80,6 @@ export default {
 				this.slices.push(this.thumbnails.slice(i, i + this.thumbnail_quantity));
 			}
 		},
-		setFocusAnimation(target, value) {
-			target.style.transition = '0.2s all ease-in-out';
-			target.style.transform = value;
-		},
-		setFocusDown() {
-			const ref = this.$refs.focus;
-				if (ref && this.active_section/10 != 9) {
-					const translateY = `translateY(${(this.module_data.height + 40) * (this.active_section/10 == 8 ? 2 : 1)}px) scale(1.03)`;
-					this.setFocusAnimation(ref, translateY);
-				}
-		},
-		setFocusUp() {
-			const ref = this.$refs.focus;
-				if (ref && this.active_section/10 != 0) {
-					const translateY = `translateY(${(this.module_data.height + 40) * (this.active_section/10 == 1 ? 0 : 1)}px) scale(1.03)`;
-					this.setFocusAnimation(ref, translateY);
-				}
-		},
-		ffCustomKeyEvent(e) {
-			if (e.key === "ArrowDown") {
-				this.setFocusDown();
-			} else if (e.key === "ArrowUp") {
-				this.setFocusUp();
-			}
-		},
-		autoKeyEvent() {
-			// const eventLeft = new KeyboardEvent('keydown', {keyCode: 37});
-			// const eventUp = new KeyboardEvent('keydown', {keyCode: 38});
-			const eventRight = new KeyboardEvent('keydown', {key: 'ArrowRight'});
-			const eventDown = new KeyboardEvent('keydown', {key: 'ArrowDown'});
-			const delay = 1500;
-			// 7
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*2)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*3)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*4)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*5)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*6)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*7)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*8)
-			// 5
-			setTimeout(()=>{
-				window.dispatchEvent(eventDown);
-				this.ffCustomKeyEvent(eventDown);
-			},delay*9)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*10)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*11)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*12)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*13)
-			// 10
-			setTimeout(()=>{
-				window.dispatchEvent(eventDown);
-				this.ffCustomKeyEvent(eventDown);
-			},delay*14)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*15)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*16)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*17)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*18)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*19)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*20)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*21)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*22)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*23)
-			// 3
-			setTimeout(()=>{
-				window.dispatchEvent(eventDown);
-				this.ffCustomKeyEvent(eventDown);
-			},delay*24)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*25)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*26)
-			//5
-			setTimeout(()=>{
-				window.dispatchEvent(eventDown);
-				this.ffCustomKeyEvent(eventDown);
-			},delay*27)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*28)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*29)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*30)
-			setTimeout(()=>{window.dispatchEvent(eventRight);},delay*31)
-			
-		},
 		handleEvent(e) {
 			if (e.key === 'Backspace') {
 				if (this.$route.path !== this.baseURL) {
@@ -173,9 +93,6 @@ export default {
 					this.reset = !this.reset;
 				});
 			}
-		},
-		updateSection(section) {
-			this.active_section = section;
 		},
 		makeIconArray() {
 			for (let i = 0; i < this.nav_text.length; i++) {
